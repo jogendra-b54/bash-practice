de33ff5806cba1b497dc8e3c45e7bbd12bd14082
jogendra-b54 <jpatro.kumar@gmail.com>
2023-08-22 19:56:03 +0530

    Auto push

--- ReverseLogic.sh
+++ ReverseLogic.sh
@@ -1,4 +1,11 @@
 s="Hello World"
 strlen=${#s}
-
-echo $strlen
+set -xv
+#echo $strlen
+for((i=$strlen-1;i>=0;i--));
+do
+revstr=$revstr${s:$i:1}
+done
+set +xv
+echo "the original string is : $s"
+echo "the original string is : $revstr"
